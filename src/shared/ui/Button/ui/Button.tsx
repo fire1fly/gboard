91392f450b2cc7b80ee4fc74e4d5db6dc71b006e
fire1fly <danyseidev@gmail.com>
2023-05-17 11:13:08 +0800

    style: add primary theme to Button

--- src/shared/ui/Button/ui/Button.tsx
+++ src/shared/ui/Button/ui/Button.tsx
@@ -4,6 +4,7 @@ import cls from './Button.module.scss';
 
 export enum ThemeButton {
   CLEAR = 'clear',
+  PRIMARY = 'primary'
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
