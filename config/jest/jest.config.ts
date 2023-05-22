540fc0d86504bfe2702d4a8ef978f553ebd0dc10
fire1fly <danyseidev@gmail.com>
2023-05-22 19:22:09 +0800

    fix: add static files support to jest, add componentRender function with MemoryRouter to testing

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -17,16 +17,17 @@ export default {
   moduleDirectories: [
     'node_modules',
   ],
-  rootDir: '../../',
   modulePaths: [
     '<rootDir>src',
   ],
-  setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
   testMatch: [
     '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
   ],
+  rootDir: '../../',
+  setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
-    '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      path.resolve(__dirname, 'jestEmptyComponent.tsx'),
   },
 };
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
-import {
-  renderWithTranslation,
-} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar tests', () => {
   test('render', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('toggle sidebar', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const sidebar = screen.getByTestId('sidebar');
     const toggleBtn = screen.getByTestId('sidebar-togglebtn');
     fireEvent.click(toggleBtn);
