2dfbdf6e4eb0dd6ffb757f8b8103e984b47f08a1
fire1fly <danyseidev@gmail.com>
2023-05-22 18:52:06 +0800

    feat: add page links with icons, upgrade collapse button

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,15 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher';
 import { useTranslation } from 'react-i18next';
+
+import { Button, ButtonSize } from 'shared/ui/Button';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink';
+import { RoutePath } from 'app/providers/router/config/routeConfig';
+
+import IconArrowLeft from 'shared/assets/icons/icon-angle-bracket-left.svg';
+import IconArrowRight from 'shared/assets/icons/icon-angle-bracket-right.svg';
+import IconPageHome from 'shared/assets/icons/icon-page-home.svg';
+import IconPageAbout from 'shared/assets/icons/icon-page-about.svg';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -23,16 +32,41 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
       data-testid="sidebar"
       className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed })}
     >
-      <button
+      <Button
         data-testid="sidebar-togglebtn"
         type="button"
         onClick={onToggle}
+        square
+        size={ButtonSize.XL}
+        className={cls.collapseBtn}
       >
-        {t('toggleButton')}
-      </button>
+        {
+          collapsed
+            ? <IconArrowRight className={cls.collapseBtn_icon} />
+            : <IconArrowLeft className={cls.collapseBtn_icon} />
+        }
+      </Button>
+      <div className={cls.nav}>
+        <AppLink
+          theme={AppLinkTheme.PRIMARY}
+          to={RoutePath.main}
+          className={cls.nav_item}
+        >
+          <IconPageHome className={cls.nav_item_icon} />
+          <span className={cls.nav_item_link}>{t('NavbarLinks:main')}</span>
+        </AppLink>
+        <AppLink
+          theme={AppLinkTheme.PRIMARY}
+          to={RoutePath.about}
+          className={cls.nav_item}
+        >
+          <IconPageAbout className={cls.nav_item_icon} />
+          <span className={cls.nav_item_link}>{t('NavbarLinks:about')}</span>
+        </AppLink>
+      </div>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        <LangSwitcher className={cls.lang} />
+        <LangSwitcher className={cls.lang} short={collapsed} />
       </div>
     </div>
   );
