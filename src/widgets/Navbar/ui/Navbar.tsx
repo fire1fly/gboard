3f94f77cffd0f111a9589713a8eeb7fd5edeb5b6
fire1fly <danyseidev@gmail.com>
2023-05-23 16:41:22 +0800

    feat: add login modal to Navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,9 @@
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink';
 import { useTranslation } from 'react-i18next';
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button';
+import { Modal } from 'shared/ui/Modal';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -11,10 +13,26 @@ interface NavbarProps {
 export const Navbar: FC<NavbarProps> = ({ className }) => {
   const { t } = useTranslation();
 
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prev) => !prev);
+  }, []);
+
   return (
     <div className={classNames(cls.Navbar)}>
       <div className={cls.links}>
-        /
+        <Button
+          className={cls.links__item}
+          theme={ButtonTheme.CLEAR}
+          size={ButtonSize.M}
+          onClick={onToggleModal}
+        >
+          {t('signIn_label')}
+        </Button>
+        <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+          {t('signIn_label')}
+        </Modal>
       </div>
     </div>
   );
