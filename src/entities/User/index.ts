3fa13df00917e7fdac56d6bf337544232650f366
fire1fly <danyseidev@gmail.com>
2023-05-27 11:46:44 +0800

    feat: add User entity init structure

---
+++ src/entities/User/index.ts
@@ -0,0 +1,9 @@
+import { userReducer, userActions } from './model/slice/userSlice';
+import { UserSchema, User } from './model/types/userSchema';
+
+export {
+  userReducer,
+  userActions,
+  User,
+  UserSchema,
+};
