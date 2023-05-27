7ea20a5594e8092c02ef6ce107e02f7c2465f593
fire1fly <danyseidev@gmail.com>
2023-05-27 20:27:24 +0800

    chore: add translations, delete comments

--- src/shared/ui/Button/ui/Button.stories.tsx
+++ src/shared/ui/Button/ui/Button.stories.tsx
@@ -4,16 +4,10 @@ import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDeco
 import { Theme } from 'app/providers/ThemeProvider';
 import { Button, ButtonSize, ButtonTheme } from './Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof Button> = {
   title: 'shared/Button',
   component: Button,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    // backgroundColor: { control: 'color' },
-  },
 };
 
 export default meta;
