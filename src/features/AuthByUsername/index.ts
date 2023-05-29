2d1896532a8bb9449a89e517058d0fc1da533dfb
fire1fly <danyseidev@gmail.com>
2023-05-29 23:25:23 +0800

    feat: add AuthByUsername feature

--- src/features/AuthByUsername/index.ts
+++ src/features/AuthByUsername/index.ts
@@ -1,1 +1,11 @@
-export { LoginModal } from './ui/LoginModal/LoginModal';
+import { LoginModal } from './ui/LoginModal/LoginModal';
+import { loginReducer } from './model/slice/loginSlice';
+import { LoginSchema } from './model/types/loginSchema';
+import { loginByUsername } from './model/services/loginByUsername/loginByUsername';
+
+export {
+  LoginModal,
+  loginReducer,
+  LoginSchema,
+  loginByUsername,
+};
