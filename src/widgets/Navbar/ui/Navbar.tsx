138cd05009ffb4e930af529b38684ee2e9834f2a
fire1fly <danyseidev@gmail.com>
2023-05-31 18:58:04 +0800

    feat: add Navbar UI on log in user

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,10 +1,10 @@
 import { FC, useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button';
-import { Modal } from 'shared/ui/Modal';
 import { LoginModal } from 'features/AuthByUsername';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserAuthData, logout } from 'entities/User';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -14,7 +14,11 @@ interface NavbarProps {
 export const Navbar: FC<NavbarProps> = ({ className }) => {
   const { t } = useTranslation();
 
+  const dispatch = useDispatch();
+
   const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const authData = useSelector(getUserAuthData);
 
   const onCloseModal = useCallback(() => {
     setIsAuthModal(false);
@@ -22,6 +26,25 @@ export const Navbar: FC<NavbarProps> = ({ className }) => {
   const onOpenModal = useCallback(() => {
     setIsAuthModal(true);
   }, []);
+  const onLogout = useCallback(() => {
+    dispatch(logout());
+  }, [dispatch]);
+
+  if (authData) {
+    return (
+      <div className={classNames(cls.Navbar)}>
+        <div className={cls.links}>
+          <Button
+            theme={ButtonTheme.OUTLINE}
+            size={ButtonSize.S}
+            onClick={onLogout}
+          >
+            {t('logout_label')}
+          </Button>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className={classNames(cls.Navbar)}>
