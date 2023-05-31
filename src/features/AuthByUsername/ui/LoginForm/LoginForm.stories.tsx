aac7dc82a04b09554ea8a56b93bbda127b751df2
fire1fly <danyseidev@gmail.com>
2023-05-31 19:00:57 +0800

    docs: add stories to LoginForm

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { LoginForm } from './LoginForm';
+
+const meta: Meta<typeof LoginForm> = {
+  title: 'features/LoginForm',
+  component: LoginForm,
+  tags: ['autodocs'],
+  decorators: [StoreDecorator({
+    loginForm: {
+      username: 'user',
+      password: 'example',
+    },
+  })],
+};
+
+export default meta;
+type Story = StoryObj<typeof LoginForm>;
+
+export const LoginFormLight: Story = {};
+export const LoginFormDark: Story = {
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
