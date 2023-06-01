e810dde80fd8950fedac87838b239d3ccc10ed9d
fire1fly <danyseidev@gmail.com>
2023-06-01 23:52:41 +0800

    feat: add reducer manager for async reducers

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,19 +1,25 @@
 import { ReducersMapObject, configureStore } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
-import { loginReducer } from 'features/AuthByUsername';
 import { StateSchema } from './StateSchema';
+import { createReducerManager } from './reducerManager';
 
 export function createReduxStore(initialState?: StateSchema) {
   const rootReducers: ReducersMapObject<StateSchema> = {
     counter: counterReducer,
     user: userReducer,
-    loginForm: loginReducer,
   };
 
-  return configureStore<StateSchema>({
+  const reducerManager = createReducerManager(rootReducers);
+
+  const store = configureStore<StateSchema>({
     reducer: rootReducers,
     devTools: __IS_DEV__,
     preloadedState: initialState,
   });
+
+  // @ts-ignore - temporary before fix types
+  store.reducerManager = reducerManager;
+
+  return store;
 }
--- src/features/AuthByUsername/index.ts
+++ src/features/AuthByUsername/index.ts
@@ -1,11 +1,9 @@
 import { LoginModal } from './ui/LoginModal/LoginModal';
-import { loginReducer } from './model/slice/loginSlice';
 import { LoginSchema } from './model/types/loginSchema';
 import { loginByUsername } from './model/services/loginByUsername/loginByUsername';
 
 export {
   LoginModal,
-  loginReducer,
   LoginSchema,
   loginByUsername,
 };
