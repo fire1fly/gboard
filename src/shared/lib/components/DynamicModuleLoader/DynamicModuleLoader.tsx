86d99c7cd7883149abae3c7fb187694dc995b7da
fire1fly <danyseidev@gmail.com>
2023-06-02 21:42:42 +0800

    feat: add dynamic module loader HOC

---
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -0,0 +1,46 @@
+import { FC, useEffect } from 'react';
+import { useDispatch, useStore } from 'react-redux';
+import { ReduxStoreWithManager, StateSchemaKey } from 'app/providers/StoreProvider/config/StateSchema';
+import { Reducer } from '@reduxjs/toolkit';
+
+export type ReducersList = {
+  [key in StateSchemaKey]?: Reducer
+}
+
+type ReducersListEntry = [StateSchemaKey, Reducer]
+
+interface DynamicModuleLoaderProps {
+  reducers: ReducersList;
+  removeAfterUnmount?: boolean;
+}
+
+export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props) => {
+  const {
+    children, reducers, removeAfterUnmount = true,
+  } = props;
+  const dispatch = useDispatch();
+  const store = useStore() as ReduxStoreWithManager;
+
+  useEffect(() => {
+    Object.entries(reducers).forEach(([key, reducer]: ReducersListEntry) => {
+      store.reducerManager.add(key, reducer);
+      dispatch({ type: `@INIT ${key} reducer` });
+    });
+
+    return () => {
+      if (removeAfterUnmount) {
+        Object.entries(reducers).forEach(([key]: ReducersListEntry) => {
+          store.reducerManager.remove(key);
+          dispatch({ type: `@DESTROY ${key} reducer` });
+        });
+      }
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      { children }
+    </>
+  );
+};
