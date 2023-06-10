a00f9cc5e25cab87d35952870ece1f4e9d9c41c4
fire1fly <danyseidev@gmail.com>
2023-06-10 23:29:02 +0800

    feat: add redux logic to Profile

---
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -0,0 +1,36 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { Profile, ProfileSchema } from '../types/profile';
+import { fetchProfileData } from '../services/fetchProfileData/fetchProfileData';
+
+const initialState: ProfileSchema = {
+  isLoading: false,
+  readonly: true,
+  error: undefined,
+  data: undefined,
+};
+
+export const profileSlice = createSlice({
+  name: 'profile',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchProfileData.pending, (state) => {
+      state.isLoading = true;
+      state.error = undefined;
+      state.data = undefined;
+    })
+      .addCase(fetchProfileData.fulfilled, (state, action: PayloadAction<Profile>) => {
+        state.isLoading = false;
+        state.error = undefined;
+        state.data = action.payload;
+      })
+      .addCase(fetchProfileData.rejected, (state, action) => {
+        state.isLoading = false;
+        state.data = undefined;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { actions: profileActions } = profileSlice;
+export const { reducer: profileReducer } = profileSlice;
