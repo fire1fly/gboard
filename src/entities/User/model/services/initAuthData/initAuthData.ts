c1f73b1527dba7f9512b5bff4c0c1019b953e75a
fire1fly <danyseidev@gmail.com>
2023-06-10 23:26:28 +0800

    chore: code support

--- src/entities/User/model/services/initAuthData/initAuthData.ts
+++ src/entities/User/model/services/initAuthData/initAuthData.ts
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { User } from 'entities/User';
-import i18n from 'shared/config/i18n/i18n';
 import { LOCALSTORAGE_USER_KEY } from 'shared/const/localstorage';
 
 export const initAuthData = createAsyncThunk<User>(
@@ -8,7 +7,7 @@ export const initAuthData = createAsyncThunk<User>(
   async (_, thunkAPI) => {
     const user = localStorage.getItem(LOCALSTORAGE_USER_KEY);
     if (!user) {
-      return thunkAPI.rejectWithValue(i18n.t('errors:userNotLogin'));
+      return thunkAPI.rejectWithValue('User not login');
     }
     return JSON.parse(user);
   },
