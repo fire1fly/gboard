5584c27767228e875048cbcae7bf64be83faef2b
fire1fly <danyseidev@gmail.com>
2023-06-13 17:31:53 +0300

    feat: add edit profile, data update in redux

--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -7,26 +7,44 @@ const initialState: ProfileSchema = {
   readonly: true,
   error: undefined,
   data: undefined,
+  form: undefined,
 };
 
 export const profileSlice = createSlice({
   name: 'profile',
   initialState,
-  reducers: {},
+  reducers: {
+    setReadonly: (state, action: PayloadAction<boolean>) => {
+      state.readonly = action.payload;
+    },
+    updateProfile: (state, action: PayloadAction<Profile>) => {
+      state.form = {
+        ...state.form,
+        ...action.payload,
+      };
+    },
+    cancelEdit: (state) => {
+      state.readonly = true;
+      state.form = state.data;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchProfileData.pending, (state) => {
       state.isLoading = true;
       state.error = undefined;
       state.data = undefined;
+      state.form = undefined;
     })
       .addCase(fetchProfileData.fulfilled, (state, action: PayloadAction<Profile>) => {
         state.isLoading = false;
         state.error = undefined;
         state.data = action.payload;
+        state.form = action.payload;
       })
       .addCase(fetchProfileData.rejected, (state, action) => {
         state.isLoading = false;
         state.data = undefined;
+        state.form = undefined;
         state.error = action.payload;
       });
   },
